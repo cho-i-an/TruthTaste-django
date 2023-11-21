c6df1704a30c23ae95f7479dac0d49b7d2a948c9
Emiii6w6 <emi6w6@gmail.com>
2023-11-20 22:25:19 -0500

    Fix Heroku deployment issues

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,10 +131,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # # Success Message
 # SUCCESS_ADD = 'You have successfully added the restaurant: "%s".'
